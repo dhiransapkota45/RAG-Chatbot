5d296f1fa3acf6b0483bc171ac412d57c3355015
dhiransapkota <dhiransapkota55@gmail.com>
2024-08-13 16:40:26 +0545

    authentication flow completed

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,10 @@
+import {
+  createServerClient,
+  parseCookieHeader,
+  serializeCookieHeader,
+} from "@supabase/ssr";
 import { Router } from "express";
+import { config } from "../config/config";
 const router = Router();
 
 router.post("/google/callback", (req, res) => {
@@ -11,6 +17,39 @@ router.post("/google/callback", (req, res) => {
     httpOnly: true,
   });
   return res.status(200).json({ message: "success" });
+});
+
+router.get("/callback", async function (req, res) {
+  console.log("reached here");
+  const code = req.query.code as string;
+  const next = (req.query.next ?? "/") as string;
+  console.log("code", code);
+  if (code) {
+    const supabase = createServerClient(
+      config.SUPABASE_URL,
+      config.SUPABASE_KEY,
+      {
+        cookies: {
+          getAll() {
+            return parseCookieHeader(req.headers.cookie ?? "");
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              res.appendHeader(
+                "Set-Cookie",
+                serializeCookieHeader(name, value, options)
+              )
+            );
+          },
+        },
+      }
+    );
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  console.log("then here");
+
+  res.redirect(303, `/${next?.slice(1)}`);
 });
 
 export default router;
